a22d2d609be6d76da39095616ee4c7cc2791a111
Cyan <cyan01100011@gmail.com>
2026-05-05 01:53:05 +0800

    add ui for actions

--- src/components/Actions.jsx
+++ src/components/Actions.jsx
@@ -1,5 +1,20 @@
+import tasksStore from "../store/tasksStore";
+
 function Actions() {
-    return ( <></> );
+  const setFilter = tasksStore((state) => state.setFilter);
+  const clearAll = tasksStore((state) => state.clearAll);
+
+  return (
+    <div>
+      <select name="filterTask" onChange={(e) => setFilter(e.target.value)}>
+        <option value="All">All</option>
+        <option value="Active">Active</option>
+        <option value="Completed">Com pleted</option>
+      </select>
+
+      <button onClick={()=> clearAll()}>Clear</button>
+    </div>
+  );
 }
 
 export default Actions;
