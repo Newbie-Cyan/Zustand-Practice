d1bc8201c5ff89e79bf33b3620a8b89101a7a293
Cyan <cyan01100011@gmail.com>
2026-05-05 03:01:52 +0800

    add logic to display the tasks

--- src/components/DisplayTask.jsx
+++ src/components/DisplayTask.jsx
@@ -1,5 +1,30 @@
+import tasksStore from "../store/tasksStore";
+
 function DisplayTask() {
-    return ( <></> );
+  const tasks = tasksStore((state) => state.tasks);
+  const removeTask = tasksStore((state) => state.removeTask);
+  const totalTask = tasks.length == 0 ? "" : tasks.length;
+
+  return (
+    <div>
+      <h2>{`Tasks: ${totalTask}`}</h2>
+
+      <div>
+        {tasks.length == 0 ? (
+          <span>no task yet..</span>
+        ) : (
+          <div>
+            {tasks.map((task, index) => (
+              <div key={index}>
+                <span>{task}</span>
+                <button onClick={() => removeTask(index)}>remove</button>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default DisplayTask;
